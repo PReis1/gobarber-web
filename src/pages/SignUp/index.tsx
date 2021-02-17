f444d31cbf5020b66f8cf07e0a1b4f3a0bb67f1d
Pedro Reis <contatodopedroreis@gmail.com>
2021-02-17 15:22:36 -0300

    error yup

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+// import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+// import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      // formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        // name: Yup.string().required('Name is required'),
+        // email: Yup.string()
+        //   .required('E-mail is required')
+        //   .email('Enter a valid e-mail adress'),
+        password: Yup.string().min(6, 'Minimum of 6 caracters'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(JSON.parse(JSON.stringify(err)));
+      // console.log(err);
+      // const errors = getValidationErrors(err);
+
+      // formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+            autoComplete="off"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
