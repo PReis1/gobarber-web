27c37b77ff5d4768d8934e2961a1a79ec1c0c2ed
Pedro Reis <contatodopedroreis@gmail.com>
2021-02-17 16:37:17 -0300

    second commit

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
-// import { FormHandles } from '@unform/core';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-// import getValidationErrors from '../../utils/getValidationErrors';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -13,15 +13,17 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
-      // formRef.current?.setErrors({});
+      formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
-        // name: Yup.string().required('Name is required'),
-        // email: Yup.string()
-        //   .required('E-mail is required')
-        //   .email('Enter a valid e-mail adress'),
+        name: Yup.string().required('Name is required'),
+        email: Yup.string()
+          .required('E-mail is required')
+          .email('Enter a valid e-mail adress'),
         password: Yup.string().min(6, 'Minimum of 6 caracters'),
       });
 
@@ -29,11 +31,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(JSON.parse(JSON.stringify(err)));
-      // console.log(err);
-      // const errors = getValidationErrors(err);
+      const errors = getValidationErrors(err);
 
-      // formRef.current?.setErrors(errors);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -43,7 +43,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={useRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
